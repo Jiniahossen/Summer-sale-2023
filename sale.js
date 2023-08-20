c00fdaccc21a4a7ee0bc0e0637e09356c6d9b651
jinia hossen <jiniahossen31@gmail.com>
2023-08-20 13:08:09 +0600

    Total price amount and product list added

--- sale.js
+++ sale.js
@@ -1,24 +1,38 @@
+let total=0;
 
-function productOnePriceAndTitle(){
-    const productOne=setProductFieldPrice('product-one-price');
-    
-   
+function productPriceAndTitle(target){
+    const selectedProductItems=document.getElementById('selected-items');
+    const productItemName=target.parentNode.childNodes[1].childNodes[3];
+    const productName=productItemName.innerText;
+    const p=document.createElement("p");
+    const count=selectedProductItems.childElementCount;
+    p.innerHTML=`${count+1}.${productName}`
+    selectedProductItems.appendChild(p);
+    p.classList.add('mb-2');
+    const productPrice=target.childNodes[7].childNodes[0];
+    const price=productPrice.innerText;
+    total=parseFloat(total)+parseFloat(price);
+    document.getElementById("total-price").innerText=total;
 }
 
+// calculate Discount  amount
 
-
-
-// Set function
-
-function setProductFieldPrice(productId){
-    const productFieldId=document.getAnimations(productId);
-    const productFieldValueText= productFieldId.innerText;
-    const productFieldValue =parseFloat(productFieldValueText);
-    return productFieldValue;
+function haveCuponButton(){
+    console.log('added');
 }
 
-// get function
+// get field amount
+function getInputFieldAmount(id) {
+    const inputFieldAmount = document.getElementById(id);
+    const inputAmount = inputFieldAmount.value;
+    const inputPrice = parseFloat(inputAmount);
+    return inputPrice;
+}
 
-function getProductPriceInTotalField(){
-    
+// set field amount
+function setInputFieldAmount(priceId,amount){
+
+    const priceValueText=document.getElementById(priceId);
+    priceValueText.innerText=amount;
+
 }
